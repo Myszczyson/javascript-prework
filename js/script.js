48070257ccd338142b49ad44f3364f832fe648fd
Dawid Myszczynski <myszczym@gmail.com>
2021-02-10 17:22:11 +0100

    Add js pc player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałeś ' + playerMove + '! Jeśli Mój ruch to kamień, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+    computerMove = 'papier';
+}
+
+if(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+if(playerInput == '2'){
+    playerMove = 'papier';
+}
+
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove)
