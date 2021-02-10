6bdd6a1830d469daae9d801969076698d73e7dca
Dawid Myszczynski <myszczym@gmail.com>
2021-02-10 18:02:09 +0100

    Add js scritp score

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ if(randomNumber == 3){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -26,6 +27,7 @@ let playerMove = 'nieznany ruch';
 if(playerInput == '1'){
     playerMove = 'kamień';
 }
+
 if(playerInput == '2'){
     playerMove = 'papier';
 }
@@ -35,3 +37,29 @@ if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove)
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Ja wygrywam!');
+} else if(computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Mamy remis!');
+} else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Ja wygrywam!');
+} else if(computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Mamy remis!');
+} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Ja wygrywam!');
+} else if(computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Mamy remis!');
+} else if(computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+    printMessage('Dawaj dawaj nie udawaj')
+} else if(computerMove == 'papier' && playerMove == 'nieznany ruch') {
+    printMessage('Dawaj dawaj nie udawaj')
+} else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
+    printMessage('Dawaj dawaj nie udawaj')
+}
