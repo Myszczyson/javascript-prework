0e06444c36b679e053435a88ffb7e79eb792cc9f
Dawid Myszczynski <myszczym@gmail.com>
2021-02-11 20:15:06 +0100

    Add js score function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
+/* PC */
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    if(argMoveId == 2){
+      return 'papier';
+    }
+    if(argMoveId == 3){
+        return 'nożyce';
+    }
+  
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
+let computerMove = getMoveName(randomNumber);
+/*
 if(randomNumber == 1){
     computerMove = 'kamień';
 }
@@ -15,15 +31,35 @@ if(randomNumber == 2){
 if(randomNumber == 3){
     computerMove = 'nożyce';
 }
+*/
 
+
+/* PLAYER */
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    if(argMoveId == 2){
+      return 'papier';
+    }
+    if(argMoveId == 3){
+        return 'nożyce';
+    }
+  
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+
+
+/*
 if(playerInput == '1'){
     playerMove = 'kamień';
 }
@@ -35,31 +71,38 @@ if(playerInput == '2'){
 if(playerInput == '3'){
     playerMove = 'nożyce';
 }
-
+*/
 printMessage('Twój ruch to: ' + playerMove)
 
-if(computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-} else if(computerMove == 'kamień' && playerMove == 'nożyce') {
-    printMessage('Ja wygrywam!');
-} else if(computerMove == 'kamień' && playerMove == 'kamień') {
-    printMessage('Mamy remis!');
-} else if(computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-} else if(computerMove == 'papier' && playerMove == 'kamień') {
-    printMessage('Ja wygrywam!');
-} else if(computerMove == 'papier' && playerMove == 'papier') {
-    printMessage('Mamy remis!');
-} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-} else if(computerMove == 'nożyce' && playerMove == 'papier') {
-    printMessage('Ja wygrywam!');
-} else if(computerMove == 'nożyce' && playerMove == 'nożyce') {
-    printMessage('Mamy remis!');
-} else if(computerMove == 'kamień' && playerMove == 'nieznany ruch') {
-    printMessage('Dawaj dawaj nie udawaj')
-} else if(computerMove == 'papier' && playerMove == 'nieznany ruch') {
-    printMessage('Dawaj dawaj nie udawaj')
-} else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
-    printMessage('Dawaj dawaj nie udawaj')
+/* SCORE */
+
+
+function displayResult(argComputerMove, argPlayerMove){
+  
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+      printMessage('Ty wygrywasz!');
+    } else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+        printMessage('Ja wygrywam!');
+    } else if(argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+        printMessage('Mamy remis!');
+    } else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+        printMessage('Ty wygrywasz!');
+    } else if(argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+        printMessage('Ja wygrywam!');
+    } else if(argComputerMove == 'papier' && argPlayerMove == 'papier') {
+        printMessage('Mamy remis!');
+    } else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+    } else if(argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+        printMessage('Ja wygrywam!');
+    } else if(argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+        printMessage('Mamy remis!');
+    } else if(argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch') {
+        printMessage('Dawaj dawaj nie udawaj')
+    } else if(argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch') {
+        printMessage('Dawaj dawaj nie udawaj')
+    } else if(argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch') {
+        printMessage('Dawaj dawaj nie udawaj')
+    }
 }
+
